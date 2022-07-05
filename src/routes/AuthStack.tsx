9624a7c9bb020e6a5bafb5a27357e9a0dd8c5f6d
Shubh0212 <shubhankar.saxena@appinventiv.com>
2022-07-05 18:09:02 +0530

    Setup sync up

--- src/routes/AuthStack.tsx
+++ src/routes/AuthStack.tsx
@@ -4,17 +4,20 @@ import Login from '../screens/auth/login';
 import OTP from '../screens/auth/otp';
 import Password from '../screens/auth/Password';
 import Names from '../utils/constant/componentNameStrings';
+import SignUp from '../screens/signup';
+import Choose_Role from '../screens/choose_role';
 
 const AuthStacks = createNativeStackNavigator();
 export default function AuthStack() {
   return (
     <AuthStacks.Navigator
       screenOptions={{headerShown: false}}
-      // initialRouteName={Names.OTP}
     >
       <AuthStacks.Screen name={Names.Login} component={Login} />
       <AuthStacks.Screen name={Names.Password} component={Password} />
       <AuthStacks.Screen name={Names.OTP} component={OTP} />
+      <AuthStacks.Screen name="SignUp" component={SignUp} />
+      <AuthStacks.Screen name="role" component={Choose_Role} />
     </AuthStacks.Navigator>
   );
 }
